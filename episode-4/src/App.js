bce42492b8e168966db40a2316adab0b67a816cb
gawasbhagyashree <gawasbhagyashree@gmail.com>
2024-08-12 04:52:46 +0530

    worked on the app

--- episode-4/src/App.js
+++ episode-4/src/App.js
@@ -1,42 +1,113 @@
+/**
+ *  COMPONENTS:
+ * Header
+ * -Logo
+ * -Nav Items
+ * 
+ * Body
+ * -Search
+ * -RestaurantContainer
+ * -RestaurantCard
+ * 
+ * Footer
+ * -Copyright
+ * -Links
+ * 
+ */
+
+
+/**
+ * Writing style components 
+ * props => arguments to a function
+ */
+
 import './App.css';
+import logo from './logo.png'
 
 
-//? REACT ELEMENT  => JS Object
+const styleCard = {
+  backgroundColor: 'yellow'
+}
 
-const jsxHeading = (
-  <h1 id='heading' className='h1'> This is react Element using JSX</h1>
-)
+export const Logo = () => {
+  return (
+    <>
+      <img src={logo} className='logo-image' alt=''></img>
+      <div className='app-name' style={styleCard}> Namaste Food Order</div>
+
+      <div style={{ backgroundColor: 'red' }}> This is inLine Style</div>
+    </>
+
+  )
+}
+
+const NavItems = () => {
+  return (
+    <>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Cart</li>
+      </ul>
+    </>
+  )
+}
 
 
-const TitleComponent = () => {
+const RestaurantCard = () => {
+  return (
+    <div className="res-card">
 
-  return <h1>This is the title </h1>
+      <div className="image">
+        <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/85cc0bea8e6459c35854eab427905c93" alt="" />
+        <h3 className='image-txt'>Offer </h3>
+      </div>
+
+      <div className="res-info">
+
+        <h3 className="res-name">Kebab Magic Roll</h3>
+
+        <div className="rating">
+          <ul>
+          <li className="rating-item rating-image">3.9</li>
+          <li className="rating-item rating-time">15-20 mins</li>
+          </ul>
+        </div>
+
+        <h4 className='cuisine'>Birayni, North-Indian</h4>
+      </div>
+
+
+
+    </div>
+  )
 
 }
 
-const number = 1000;
+const RestaurantContainer = () => {
+  return (
+    <div className="res-container">
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+      <RestaurantCard />
+
+    </div>
+
+  )
+}
+
+export const Header = () => {
+  return (
+    <>
+      <Logo />
+      <NavItems />
+      <RestaurantContainer />
+    </>
+  )
+
+}
 
 
-//? REACT COMPONENT  => JS Function
 
-const HeadingComponent = () => (
-  <div id='container'>
-
-    <TitleComponent />
-
-    {TitleComponent()}     {/* OR Component inside componentn  using {}  */}
-
-
-    {jsxHeading}            {/* Element inside component using {}  */}
-    <h1>Namaste react Functional Component </h1>
-
-    {number}
-
-
-  </div>
-
-)
-
-
-
-export default (<HeadingComponent/>)
+export default (<Header />)
