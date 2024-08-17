3d15ff937ddf2f213da83f157a4a5749a21e1c1f
gawasbhagyashree <gawasbhagyashree@gmail.com>
2024-08-18 03:50:54 +0530

    created episode-5

---
+++ episode-5/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Header from './App';  // HeadingComponent is a default export
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <div>
+        {Header}
+    </div>
+);
+
